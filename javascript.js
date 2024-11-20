5115cb4e8d3e3ce739f7339eb8bbaae28e70ce18
unknown <umerfsniper@gmail.com>
2024-11-20 05:46:58 -0800

    message

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// alert("Smoking is Dangerious for health")
+// alert("Smoking is Dangerious for health")
+// confirm("Do you want to quit the browser")
+// prompt("Input Box")
+// document.write("Hello World")
+// console.log("Hello WOrld");
+// let a="56546546"
+// a="DWD-14"
+// document.write(a)
+let val=5
+let val2=3
+val2=val
+val=3
+val2=val
+document.write(val2)
+
+
+
+
+
